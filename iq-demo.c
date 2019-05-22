bf58dc7dc27fdab9c4c4424b9a9bb3233b03b494
Chuck McManis <cmcmanis@mcmanis.com>
2019-05-22 08:14:31 -0700

    added ffts to iq-demo, now to add fir

--- iq-demo.c
+++ iq-demo.c
@@ -17,31 +17,37 @@
 #include <math.h>
 #include <complex.h>
 #include "signal.h"
+#include "fft.h"
 
 #define SAMPLE_RATE 8192
+#define BINS 1024
 
 int
 main(int argc, char *argv[]) {
 	sample_buffer 	*r;
 	sample_buffer	*iq;
 	sample_buffer	*iq2;
+	sample_buffer	*fft1;
+	sample_buffer	*fft2;
 	double	diffs[SAMPLE_RATE];
 	FILE	*of;
 
 
-	r = alloc_buf(SAMPLE_RATE * 4, SAMPLE_RATE);
+	r = alloc_buf(SAMPLE_RATE * 4, 4 * SAMPLE_RATE);
 	iq = alloc_buf(SAMPLE_RATE, SAMPLE_RATE);
 	iq2 = alloc_buf(SAMPLE_RATE, SAMPLE_RATE);
 
 	/* this is a complex sample */
-	add_cos(iq, 100.0, 1.0);
+	add_cos(iq, 512.0, 1.0);
 
 	/* this is a real signal, oversampled by 4 */
-	add_cos_real(r, 100.0, 1.0);
+	add_cos_real(r, 512.0, 1.0);
 	/* and we decimate by 4 and pull off adjacent samples */
 	for (int i = 0; i < SAMPLE_RATE * 4; i += 4) {
-		iq2->data[i/4] = creal(r->data[i+1]) + creal(r->data[i]) * I;
+		iq2->data[i/4] = creal(r->data[i]) + creal(r->data[i+2]) * I;
 	}
+	fft1 = compute_fft(iq, BINS, W_BH);
+	fft2 = compute_fft(iq2, BINS, W_BH);
 	of = fopen("./plots/iq-demo.plot", "w");
 	if (of == NULL) {
 		fprintf(stderr, "Could not open output file\n");
@@ -59,14 +65,19 @@ main(int argc, char *argv[]) {
 			cimag(iq->data[i]) - cimag(iq2->data[i]));
 	}
 	fprintf(of, "EOD\n");
-	fprintf(of, "set xlabel \"Time (samples)\"\n"
-			    "set ylabel \"Amplitude\"\n"
+	fprintf(of, "$fft_plt<<EOFFT\n");
+	fprintf(of, "bin IQ1 IQ2\n");
+	for (int i = 0; i < BINS; i++) {
+		fprintf(of, "%d %f %f\n", i, cmag(fft1->data[i]), cmag(fft2->data[i]));
+	}
+	fprintf(of, "EOFFT\n");
+	fprintf(of, "set multiplot layout 4, 1\n"
 				"set key outside autotitle columnheader\n"
-				"plot [0:1024] $my_plot using 1:2 with lines, \\\n"
-				"    \"\" using 1:3 with lines, \\\n"
-				"    \"\" using 1:4 with lines, \\\n"
-				"    \"\" using 1:5 with lines, \\\n"
-				"    \"\" using 1:6 with lines, \\\n"
-				"    \"\" using 1:7 with lines, \\\n");
+				"plot [0:110] $my_plot using 1:2 with lines, \\\n"
+				"    \"\" using 1:3 with lines\n"
+				"plot [0:110] $my_plot using 1:4 with lines, \\\n"
+				"    \"\" using 1:5 with lines\n"
+				"plot [0:1024] $fft_plt using 1:2 with lines\n"
+				"plot [0:1024] $fft_plt using 1:3 with lines\n");
 	fclose(of);
 }
